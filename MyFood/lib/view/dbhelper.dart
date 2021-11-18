3b4269fe9b7bf222d729813706c01f1cff821fc8
aadi-1111 <adityathakkar2003@gmail.com>
2021-11-18 11:29:08 -0500

    add

--- MyFood/lib/view/dbhelper.dart
+++ MyFood/lib/view/dbhelper.dart
@@ -4,14 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class DatabaseHelper {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-<<<<<<< HEAD
-  Future<bool> createNewTask(String food, String? userId) async {
-    try {
-      await _firestore.collection("Users").doc(userId).collection("Todos").doc().set({
-        "Food Item": food,
-=======
-  Future<bool> createNewTask(
-      String food, String location, String? userId) async {
+  Future<bool> createNewTask(String food, String location,
+      String? userId) async {
     try {
       await _firestore
           .collection("Users")
@@ -21,15 +15,10 @@ class DatabaseHelper {
           .set({
         "Food Item": food,
         "Purchase Location": location,
->>>>>>> upstream/master
       });
       return true;
     } catch (e) {
       return false;
     }
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> upstream/master
